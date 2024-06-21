f07ef8c25ee27cbcdebe498e3916288668e65562
emrahhorsunlu <73930484+emrahhorsunlu@users.noreply.github.com>
2024-06-21 19:46:05 +0300

    Timer Add

--- Assets/Scripts/CardController.cs
+++ Assets/Scripts/CardController.cs
@@ -1,14 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class CardController : MonoBehaviour
 {
+
     public GameObject warrior, archer, cavalry;
+    public Button card1Btn, card2Btn, card3Btn;
+    public float cooldownTime = 10f;  // Bekleme süresi (10 saniye)
+    private float currentCooldownTime; // Mevcut kalan süre
+
     // Start is called before the first frame update
     void Start()
     {
         PlayerPrefs.SetString("StageArmy", "Bronze");
+        card1Btn = GameObject.Find("Card1").GetComponent<Button>();
+        card2Btn = GameObject.Find("Card2").GetComponent<Button>();
+        card3Btn = GameObject.Find("Card3").GetComponent<Button>();
     }
 
     // Update is called once per frame
@@ -18,22 +28,27 @@ public class CardController : MonoBehaviour
     }
     public void OnCardButtonClicked(string buttonName)
     {
+
         if (PlayerPrefs.GetString("StageArmy") == "Bronze")
         {
-            if (buttonName == "Card1")
+
+            if (buttonName == "Card1" && card1Btn.interactable == true)
             {
-                Debug.Log(buttonName + " button clicked!" + "Bronz Çağ");
+                //Debug.Log(buttonName + " button clicked! " + PlayerPrefs.GetString("StageArmy"));
                 SpawnCapsule(buttonName);
+                StartCoroutine(ButtonCooldown(card1Btn));
             }
-            else if (buttonName == "Card2")
+            else if (buttonName == "Card2" && card2Btn.interactable == true)
             {
-                Debug.Log(buttonName + " button clicked!" + "Bronz Çağ");
+                //Debug.Log(buttonName + " button clicked! " + PlayerPrefs.GetString("StageArmy"));
                 SpawnCapsule(buttonName);
+                StartCoroutine(ButtonCooldown(card2Btn));
             }
-            else if (buttonName == "Card3")
+            else if (buttonName == "Card3" && card3Btn.interactable == true)
             {
-                Debug.Log(buttonName + " button clicked!" + "Bronz Çağ");
+                //Debug.Log(buttonName + " button clicked! " + PlayerPrefs.GetString("StageArmy"));
                 SpawnCapsule(buttonName);
+                StartCoroutine(ButtonCooldown(card3Btn));
             }
 
         }
@@ -61,6 +76,33 @@ public class CardController : MonoBehaviour
             Quaternion spawnRotation = Quaternion.identity;   // Rotasyon olmadan başlangıç rotasyonu
 
             GameObject capsule = Instantiate(cavalry, spawnPosition, spawnRotation);
+        }
+
+    }
+
+    IEnumerator ButtonCooldown(Button myButton)
+    {
+        if (myButton.interactable)
+        {
+            Debug.Log("Tıklanan Buton Adı: " + myButton.gameObject.name);
+            // Butonu devre dışı bırak
+            myButton.interactable = false;
+            myButton.enabled = false;
+            currentCooldownTime = cooldownTime;
+
+            // Kalan süreyi göster
+            while (currentCooldownTime > 0)
+            {
+                // Kalan süreyi TextMeshPro labeline yazdır
+                myButton.GetComponentInChildren<TextMeshProUGUI>().text = currentCooldownTime.ToString("F1") + "s";
+                currentCooldownTime -= Time.deltaTime;
+                yield return null; // Bir sonraki frame'e kadar bekle
+            }
+
+            // Süre doldu, butonu tekrar aktif hale getir ve labeli sıfırla
+            myButton.interactable = true;
+            myButton.enabled = true;
+            myButton.GetComponentInChildren<TextMeshProUGUI>().text = " ";
         }
 
     }
